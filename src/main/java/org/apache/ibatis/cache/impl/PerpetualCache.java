d9ac19c6dfa598fa40a3ec3b1f65ed9f98183f95
qiaok <qiaok@superred.com.cn>
2021-09-01 18:20:31 +0800

    1.Mybatis Default cache is PerpetualCache ,it contained hashmap

--- src/main/java/org/apache/ibatis/cache/impl/PerpetualCache.java
+++ src/main/java/org/apache/ibatis/cache/impl/PerpetualCache.java
@@ -22,6 +22,8 @@ import org.apache.ibatis.cache.Cache;
 import org.apache.ibatis.cache.CacheException;
 
 /**
+ * It is HashMap
+ * @author qiaok
  * @author Clinton Begin
  */
 public class PerpetualCache implements Cache {
